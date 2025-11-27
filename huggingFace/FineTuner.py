58e0106875ea7605650738b338039547141ffa68
kissinger <kchen0723@gmail.com>
2025-11-27 14:29:36 -0800

    fix bugs

--- huggingFace/FineTuner.py
+++ huggingFace/FineTuner.py
@@ -24,16 +24,16 @@ class FineTuner:
 
         class CustomDataset(torch.utils.data.Dataset):
             def __init__(self, encodings, labels):
-                self.encoding = encodings
+                self.encodings = encodings
                 self.labels = labels
 
             def __getitem__(self, idx):
-                item = {key: val[idx] for key, val in self.encoding.items()}
+                item = {key: val[idx] for key, val in self.encodings.items()}
                 item["labels"] = torch.tensor(self.labels[idx])
                 return item
             
             def __len__(self):
-                return len(self.lables)
+                return len(self.labels)
 
         return CustomDataset(encodings, labels)
     
